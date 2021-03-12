36456281c2bcec6b3d3d1dba1502df7debc5a4b2
rafsimo <simona-b@outlook.sk>
2021-03-12 16:29:11 +0000

    fix: updated csv extract

--- src/file_handlers/csv.py
+++ src/file_handlers/csv.py
@@ -1,30 +1,17 @@
 import csv
 path = '/workspace/data/2021-02-23-isle-of-wight.csv'
+
 def import_csv(cached_list):
     try:
         with open(path) as file:
             fieldnames = ['date', 'location', 'full_name', 'order', 'payment_type', 'total', 'card_details']
             new_file = csv.DictReader(file, delimiter = ',', fieldnames=fieldnames)
             for row in new_file:
-                cached_list.append(row)
+                date = str(row['date'])
+                location = str(row['location'])
+                order = str(row['order'])
+                total = float(row['total'])
+                cached_list.append({'date':date, 'location':location, 'order':order, 'total':total})
             return cached_list
     except Exception as e:
         print('An error occurred: ' + str(e))
-    
-
-# def import_csv(file_path, csv_file):
-#     try:
-#         with open(file_path) as file:
-#             new_file = csv.DictReader(file, delimiter = ',')
-#             for row in new_file:
-#                 idx = int(row['id'])
-#                 name = str(row['customer_name'])
-#                 address = str(row['customer_address'])
-#                 phone = str(row['customer_phone'])
-#                 courier = int(row['courier'])
-#                 status = str(row['status'])
-#                 item = [int(i) for i in row['items'][1:-1].split(', ') if i.isnumeric() == True]
-#                 csv_file.append({'id':idx, 'customer_name':name, 'customer_address':address, 'customer_phone':phone, 'courier':courier, 'status':status, 'items':item})
-#         return csv_file
-#     except Exception as e:
-#         print('An error occurred: ' + str(e))
