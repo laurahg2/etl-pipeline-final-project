4ea46b59bd5cf32e6e7e6522c54f5088e3628e59
rafsimo <simona-b@outlook.sk>
2021-03-22 15:58:05 +0000

    refactor: split extract script into smaller chunks

--- src/extract.py
+++ src/extract.py
@@ -1,76 +1,69 @@
 
-def clear_orders(orders_list):
+def get_to_products(orders_list: list):
     for order in orders_list:
         for key, value in order.items():
             if key == 'order':
                 products = value
-                if products[0] == ",":
-                    products = products[1:]
-                if "Large," in products:
-                    products = products.replace("Large,", "Large-")
-                if "Regular," in products:
-                    products = products.replace("Regular,", "Regular-")
-                if ",," in products:
-                    products = products.replace(",,", ",")
+    return products
+
+
+def clear_orders(orders_list: list):
+    products = get_to_products(orders_list)
+    if products[0] == ",":
+        products = products[1:]
+    if "Large," in products:
+        products = products.replace("Large,", "Large-")
+    if "Regular," in products:
+        products = products.replace("Regular,", "Regular-")
+    if ",," in products:
+        products = products.replace(",,", ",")
+    for order in orders_list:
         order['order'] = products
+
     return orders_list
 
 
-def create_orders_dictionary(orders_list):
+def create_orders_dictionary(orders_list: list):
+    products = get_to_products(orders_list)
+    count_commas = products.count(",")
+    product_string = products.split(",", count_commas)
+    list_order = []
+    n = 0
+    while n in range(count_commas):
+        product_name = product_string[n]
+        product_price = product_string[n+1]
+        n += 2
+        if "Large-" in product_name:
+            product_size = product_name[:5]
+            product_name = product_name[6:]
+        elif "Regular-" in product_name:
+            product_size = product_name[:7]
+            product_name = product_name[8:]
+        else:
+            product_size = "Standard"
+        dictionary = {
+                "product_name": product_name,
+                "product_size": product_size,
+                "product_price": product_price
+            }
+        list_order.append(dictionary)
     for order in orders_list:
-        for key, value in order.items():
-            if key == 'order':
-                list_order = []
-                products = value
-                number = products.count(",")
-                product_data = products.split(",", number)
-                n = 0
-                while n in range(number):
-                    product_name = product_data[n]
-                    product_price = product_data[n+1]
-                    n += 2
-
-                    if "Large-" in product_name:
-                        size = product_name[:5]
-                        name = product_name[6:]
-                        dictionary = {
-                            "product_name": name,
-                            "product_price": product_price,
-                            "product_size": size
-                        }
-
-                    elif "Regular-" in product_name:
-                        size = product_name[:7]
-                        name = product_name[8:]
-                        dictionary = {
-                            "product_name": name,
-                            "product_price": product_price,
-                            "product_size": size
-                        }
-
-                    else:
-                        dictionary = {
-                            "product_name": product_name,
-                            "product_price": product_price,
-                            "product_size": "Standard"
-                        }
-                    list_order.append(dictionary)
         order['order'] = list_order
     return orders_list
 
 
-def extract_unique_names(orders_list):
-    duplicates_removed = []
-    for order in clear_orders:
-        list_to_string = ''.join(order)
-        number = list_to_string.count(",")
-        product = order.split(",", number)
-        n = 0
-        while n in range(number):
-            name = product[n]
-            price = product[n+1]
-            n += 2
-            product_string = name + "," + price
-            if product_string not in duplicates_removed:
-                duplicates_removed.append(product_string)
-    return duplicates_removed
+# def extract_unique_names(orders_list):
+#     duplicates_removed = []
+#     for order in clear_orders:
+#         list_to_string = ''.join(order)
+#         count_commas = list_to_string.count(",")
+#         product = order.split(",", count_commas)
+#         n = 0
+#         while n in range(count_commas):
+#             name = product[n]
+#             price = product[n+1]
+#             n += 2
+#             product_string = name + "," + price
+#             if product_string not in duplicates_removed:
+#                 duplicates_removed.append(product_string)
+#     return duplicates_removed
