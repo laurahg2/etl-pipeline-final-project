e3730e502c054929ae02c68e4925dc4207138d97
MrIgumnov96 <dennis.igumnov@yahoo.com>
2021-03-22 15:49:43 +0000

    feat: completed database functions to insert into tables

--- src/app.py
+++ src/app.py
@@ -16,26 +16,9 @@ if __name__ == "__main__":
     extract.clear_orders(new_list)
     extract.create_orders_dictionary(new_list)
     # extract.extract_unique_names(new_list)
-    # print(new_list)
-    # for order in new_list:
-    #     for key, value in order.items():
-    #         if key == "order":
-    #             orders_list = value
-    #             for v in orders_list:
-    #                 print(v["product_name"])
-    # for order in new_list:
-    #     for key, value in order.items():
-    #         if key == 'date':
-    #             date = value
-    #             print(date)
-    #         if key == 'time':
-    #             time = value
-    #             print(time)
+
 connection.create_table()                
-# connection.add_product_to_database(new_list)
-# connection.add_location_to_database(new_list)
-# connection.add_transaction_to_database(new_list)
-connection.fetch_products_data()
-connection.load_into_basket_table_and_update_local_ids(new_list)
-connection.load_into_products_in_basket_table(new_list)
-# connection.add_basket_to_database(new_list)
+connection.add_product_to_database(new_list)
+connection.add_location_to_database(new_list)
+connection.add_transaction_to_database(new_list)
+connection.add_basket_to_database(new_list)
